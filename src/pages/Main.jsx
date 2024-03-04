cb0f8e287945290704a4122664bf164ae288f12d
cvanstaen <clement.van.staen@zalando.de>
2024-03-04 21:19:12 +0100

    formating WIP

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Tooltip } from "antd";
+import { Carousel } from 'antd';
 import { useTranslation } from "react-i18next";
-
-import EmailLogo from "../img/logos/emailLogo.png";
 
 import './Main.less';
 
@@ -17,27 +15,26 @@ export const Main = () => {
             <div className="main__title">Spielbetrieb</div>
             <div className="main__subtext">love to love</div>
             <div className='main__text'>
-                Spielbetrieb ist ein ganzheitliches, sexpositives Erlebnis. Wir sind eine Digital-Agentur spezialisiert auf Kommunikation, PR, Eventmanagement sowie Daten- & Jugendschutz.
-
-            Zu unseren Leistungen gehören:
-
-                - PR & Kommunikation: Wir stehen unseren Partnern beratend zur Seite und übernehmen auch die komplette Kommunikation.
-
-                - Marketing & Werbung: Von online Werbung bis hin zu Streuartikel haben wir deine Marke im Griff.
-
-                - Daten- & Jugendschutz: Wir kümmern uns darum, damit du ruhig schlafen kannst.
-
-            Zusätzlich bieten wir Live-Events und Eventmanagement an:
-
-                - Einlasskontrolle: ein erfahrenes Team an deiner Tür.
-                - Spielmöbel: miete, einzelne Spielmöbel oder ganze Pop-up Dungeons.
-                - DJs: die passende Musik für deine Party.
-
-            Unsere Events
-
-                - Damenwahl - FemDom Spieleabend
-                - Happy Fisting Club - Anale Liebe, von soft bis extrem
-                - Harness Madness - Hedonist*innen im Harness
+                Spielbetrieb ist ein ganzheitliches, sexpositives Erlebnis.<br/><br/>
+                Wir sind eine Digital-Agentur spezialisiert auf Kommunikation, PR, Eventmanagement sowie Daten- & Jugendschutz.
+                    <div className='main__carouselElement'>
+                        <div className='main__carouseltitle'>Zu unseren Leistungen gehören</div>
+                        <div className='main__carouselSubElement'>PR & Kommunikation: Wir stehen unseren Partnern beratend zur Seite und übernehmen auch die komplette Kommunikation.</div>
+                        <div className='main__carouselSubElement'>Marketing & Werbung: Von online Werbung bis hin zu Streuartikel haben wir deine Marke im Griff.</div>
+                        <div className='main__carouselSubElement'>Daten- & Jugendschutz: Wir kümmern uns darum, damit du ruhig schlafen kannst.</div>
+                    </div>
+                    <div className='main__carouselElement'>
+                        <div className='main__carouseltitle'>Zusätzlich bieten wir Live-Events und Eventmanagement an</div>
+                        <div className='main__carouselSubElement'>Einlasskontrolle: ein erfahrenes Team an deiner Tür.</div>
+                        <div className='main__carouselSubElement'>Spielmöbel: miete, einzelne Spielmöbel oder ganze Pop-up Dungeons.</div>
+                        <div className='main__carouselSubElement'>DJs: die passende Musik für deine Party.</div>
+                    </div>
+                    <div className='main__carouselElement'>
+                        <div className='main__carouseltitle'>Unsere Events</div>
+                        <div className='main__carouselSubElement'>Damenwahl - FemDom Spieleabend</div>
+                        <div className='main__carouselSubElement'>Happy Fisting Club - Anale Liebe, von soft bis extrem</div>
+                        <div className='main__carouselSubElement'>Harness Madness - Hedonist*innen im Harness</div>
+                    </div>
             </div>
         </div>
         </>
